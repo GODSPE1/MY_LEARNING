5f744d31ed0316ad50f15af81ff62073378e958f
GODSPE1 <godspy18@gmail.com>
2025-10-20 23:37:37 +0100

    feat: add initial implementation of for-of loop with subjects array

---
+++ Javascript/for-of-loop.js
@@ -0,0 +1,7 @@
+const subjects = ['English', 'Mathematics', 'Physics', 'Chemistry', 'Biology'];
+    subjects.forEach((subject) => {
+    })
+console.log(subjects);
+    // for (const i = 0; i < subjects.length; i++) {
+//     console.log(i);
+// }
