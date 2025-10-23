488d12cecbe9fd9f7df27d64c7e45b2b26fec2eb
GODSPE1 <godspy18@gmail.com>
2025-10-23 08:13:00 +0100

    deeper understanding  on the creating of Elements and using refactoring functions

--- Javascript/creating-elememnts in DOM.js
+++ Javascript/creating-elememnts in DOM.js
@@ -14,5 +14,66 @@ let text = document.createTextNode('Hello world');
 div.appendChild(text);
 
 document.body.appendChild(div);
-document.querySelector('.item').appendChild(div);
+// document.querySelector('.item').appendChild(div);
 // console.log(div)
+
+
+
+function createNewItem(item) {
+  const li = document.createElement('li');
+
+  li.appendChild(document.createTextNode(item));
+
+  const button = document.createElement('button');
+
+  button.className = 'remove-item btn-link text-red';
+
+  const icon = document.createElement('i');
+
+  icon.className = 'fa-solid fa-xmark';
+
+  button.appendChild(icon);
+
+  li.appendChild(button);
+
+  document.querySelector('.items').appendChild(li);
+}
+
+
+
+
+function createNewItem2(item) {
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(item));
+
+  const button = createButton('remove-item btn-link text-red');
+  
+  
+  li.appendChild(button);
+  
+  document.querySelector('.items').appendChild(li);
+}
+
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+  
+}
+
+function createIcon(icon) {
+  const icon1 = document.createElement('i');
+  icon1.className = icon;
+  return icon1;
+  
+}
+
+createNewItem('Cheese');
+createNewItem2('Fish');
+
+
+
